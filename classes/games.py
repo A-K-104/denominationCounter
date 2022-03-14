7978b93b717dfba82c120ebedda8ff0f4d96b851
Asaf Kostiner <kostinerasaf@gmail.com>
2022-03-14 21:49:58 +0200

    some bug fix new

--- classes/games.py
+++ classes/games.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 from flask import session
-
 import constance
 
 db = constance.db
@@ -11,7 +9,7 @@ class Games(db.Model):
     # to init db in terminal type: python ->from app import db->db.create_all()-> exit(). and you are set!
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    points = db.Column(db.JSON(),default={}, nullable=True)
+    points = db.Column(db.JSON(), default={}, nullable=True)
     active = db.Column(db.Boolean(), default=False, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
--- classes/stations.py
+++ classes/stations.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import session
+
 import constance
 
 db = constance.db
