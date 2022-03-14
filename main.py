5f4b701c140b87a716bed9a9a62c2433c39da5a1
Asaf Kostiner <kostinerasaf@gmail.com>
2022-03-15 00:51:18 +0200

    HOPE?!?!

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import constance
 from routes.basicRoutsHandling import basic_routs_handling
 
-db = constance.db
-app = constance.app
-sess = constance.sess
-app.register_blueprint(basic_routs_handling)
 
+def creat():
+    db = constance.db
+    app = constance.app
+    app.register_blueprint(basic_routs_handling)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
+    return app
 #
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000)
