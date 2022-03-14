7b4c2b0685e8a86a6a41ff30f78945e44ae82bd6
Asaf Kostiner <kostinerasaf@gmail.com>
2022-03-14 22:09:48 +0200

    some bug fix new

--- __init__.py
+++ __init__.py
@@ -4,19 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 import constance
 from routes.basicRoutsHandling import basic_routs_handling
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'LJADLKCDDFD425344dfhW'
-app.config["SESSION_PERMANENT"] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gameData.db'
-app.config['SQLALCHEMY_BINDS'] = {'stations': 'sqlite:///gameData.db', 'games': 'sqlite:///gameData.db'}
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-# db = constance.db
-# app = constance.app
-# sess = constance.sess
-# app.register_blueprint(basic_routs_handling)
+app = constance.app
+db = constance.db
+app = constance.app
+sess = constance.sess
+app.register_blueprint(basic_routs_handling)
 
 
 @app.before_first_request
