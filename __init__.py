009b6412a413672f673859270e38d4e2a3a71225
Asaf Kostiner <kostinerasaf@gmail.com>
2022-03-14 22:28:06 +0200

    HOPE!

--- __init__.py
+++ __init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import constance
-from routes.basicRoutsHandling import basic_routs_handling
-
-app = constance.app
-db = constance.db
-sess = constance.sess
-app.register_blueprint(basic_routs_handling)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
+# import constance
+# from routes.basicRoutsHandling import basic_routs_handling
+#
+# app = constance.app
+# db = constance.db
+# sess = constance.sess
+# app.register_blueprint(basic_routs_handling)
+#
+#
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
+from app import app
 
 if __name__ == "__main__":
     app.run()
